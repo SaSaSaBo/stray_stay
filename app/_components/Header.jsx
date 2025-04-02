597b9348b43b0e1f26fa2277e1326688ff393f16
SaSaSaBo <sasasabo14@gmail.com>
2025-04-02 15:10:40 +0300

    in the beninging + headers + colors

---
+++ app/_components/Header.jsx
@@ -0,0 +1,277 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import { Moon, Sun } from "@deemlol/next-icons";
+{/* headlessui import */ }
+import {
+  Dialog,
+  DialogPanel,
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Popover,
+  PopoverButton,
+  PopoverGroup,
+  PopoverPanel,
+} from '@headlessui/react'
+{/* headlessui import end */ }
+
+{/* heroicons import */ }
+import {
+  ArrowPathIcon,
+  Bars3Icon,
+  ChartPieIcon,
+  CursorArrowRaysIcon,
+  FingerPrintIcon,
+  SquaresPlusIcon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline'
+import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
+import Image from 'next/image'
+{/* heroicons import end */ }
+
+{/* products */ }
+const products = [
+  { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
+  { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
+  { name: 'Security', description: 'Your customers’ data will be safe and secure', href: '#', icon: FingerPrintIcon },
+  { name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
+  { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
+]
+
+{/* callsToAction */ }
+const callsToAction = [
+  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
+  { name: 'Contact sales', href: '#', icon: PhoneIcon },
+]
+
+{/* Header Start */ }
+const Header = () => {
+
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [isNightMode, setIsNightMode] = useState(true);
+
+  const handleToggle = () => {
+    setIsNightMode(!isNightMode); // Durumu değiştir
+  };
+
+  // Sayfa yüklendiğinde, localStorage'dan tema tercihini kontrol et
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme) {
+      setIsNightMode(storedTheme === "dark");
+    }
+  }, []);
+
+  // Sayfa yenilendiğinde veya tema değiştiğinde, localStorage'a tema tercihini kaydet
+  useEffect(() => {
+    if (isNightMode) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [isNightMode]);
+
+  return (
+    <header className="bg-white">
+
+      {/* Global Web Nav */}
+      <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+
+        {/* logo side */}
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <Image
+              alt=""
+              src="/skz_sty/skz_logo2.png"
+              className=""
+              width={120}
+              height={80}
+            />
+          </a>
+        </div>
+
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            onClick={() => setMobileMenuOpen(true)}
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon aria-hidden="true" className="size-6" />
+          </button>
+        </div>
+        {/* logo side end */}
+
+        {/* Bar Sections */}
+        <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+
+          {/* Product section */}
+          {/* <Popover className="relative">
+            <PopoverButton className="flex items-center gap-x-1 text-sm/6 font-semibold text-gray-900">
+              Product
+              <ChevronDownIcon aria-hidden="true" className="size-5 flex-none text-gray-400" />
+            </PopoverButton>
+
+            <PopoverPanel
+              transition
+              className="absolute top-full -left-8 z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white ring-1 shadow-lg ring-gray-900/5 transition data-closed:translate-y-1 data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-150 data-leave:ease-in"
+            >
+              <div className="p-4">
+                {products.map((item) => (
+                  <div
+                    key={item.name}
+                    className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm/6 hover:bg-gray-50"
+                  >
+                    <div className="flex size-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                      <item.icon aria-hidden="true" className="size-6 text-gray-600 group-hover:text-indigo-600" />
+                    </div>
+                    <div className="flex-auto">
+                      <a href={item.href} className="block font-semibold text-gray-900">
+                        {item.name}
+                        <span className="absolute inset-0" />
+                      </a>
+                      <p className="mt-1 text-gray-600">{item.description}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
+                {callsToAction.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="flex items-center justify-center gap-x-2.5 p-3 text-sm/6 font-semibold text-gray-900 hover:bg-gray-100"
+                  >
+                    <item.icon aria-hidden="true" className="size-5 flex-none text-gray-400" />
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            </PopoverPanel>
+          </Popover> */}
+          {/* Product section end */}
+
+          <a href="#" className="text-lg font-medium text-gray-900">Members</a>
+
+          <a href="#" className="text-lg font-medium text-gray-900">SKZOO</a>
+
+          <a href="#" className="text-lg font-medium text-gray-900">Albums</a>
+
+          <a href="#" className="text-lg font-medium text-gray-900">Records</a>
+
+          <a href="#" className="text-lg font-medium text-gray-900 transition-all">Content</a>
+
+        </PopoverGroup>
+        {/* Bar Sections End */}
+
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <a
+            href="#"
+            className="text-sm/6 font-semibold text-gray-900"
+            onClick={handleToggle}
+          >
+            {isNightMode ? (
+              <Moon size={24} color="#000000" />
+            ) : (
+              <Sun size={24} color="#000000" />
+              // <Sun size={24} color="#FFFFFF" />
+            )}
+          </a>
+        </div>
+
+        {/* Login Button */}
+        {/* <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+            Log in <span aria-hidden="true">&rarr;</span>
+          </a>
+        </div> */}
+        {/* Login Button End */}
+
+      </nav>
+      {/* Global Web Nav End */}
+
+      {/* Mobile Menu Dialog */}
+      <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+        <div className="fixed inset-0 z-10" />
+        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+
+          {/* Menu Icon Bar */}
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img
+                alt=""
+                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                className="h-8 w-auto"
+              />
+            </a>
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(false)}
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon aria-hidden="true" className="size-6" />
+            </button>
+          </div>
+          {/* Menu Icon Bar End */}
+
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+
+              {/* Menu Bar Secttion */}
+              <div className="space-y-2 py-6">
+
+                {/* Product Section */}
+                <Disclosure as="div" className="-mx-3">
+                  <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                    Product
+                    <ChevronDownIcon aria-hidden="true" className="size-5 flex-none group-data-open:rotate-180" />
+                  </DisclosureButton>
+                  <DisclosurePanel className="mt-2 space-y-2">
+                    {[...products, ...callsToAction].map((item) => (
+                      <DisclosureButton
+                        key={item.name}
+                        as="a"
+                        href={item.href}
+                        className="block rounded-lg py-2 pr-3 pl-6 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50"
+                      >
+                        {item.name}
+                      </DisclosureButton>
+                    ))}
+                  </DisclosurePanel>
+                </Disclosure>
+                {/* Product Section End */}
+
+                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Features</a>
+
+                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Marketplace</a>
+
+                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Company</a>
+
+              </div>
+              {/* Menu Bar Secttion End */}
+
+              {/* Login Button */}
+              <div className="py-6">
+                <a
+                  href="#" className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Log in</a>
+              </div>
+              {/* Login Button End */}
+
+            </div>
+          </div>
+
+        </DialogPanel>
+      </Dialog>
+      {/* Mobile Menu Dialog End */}
+
+    </header>
+  )
+}
+
+export default Header;

6baa79a80064d57d05cf7afa64b4440bf8eff9ff
SaSaSaBo <sasasabo14@gmail.com>
2025-04-04 14:05:37 +0300

    Banner kısmı bitti

--- app/_components/Banner.jsx
+++ app/_components/Banner.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { UserRef, UseStage } from "react";
+import Image from "next/image"; // Image bileşeni doğru şekilde import edildi.
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination } from "swiper"; // Navigation modülünü de ekledik
 import "swiper/css";
@@ -10,18 +11,123 @@ import "swiper/css/navigation"; // Navigation için Swiper CSS
 const Banner = () => {
     return (
         <>
-            <div className="container">
-                <Swiper
-                    spaceBetween={30}
-                    centeredSlides={true} // Ortalamayı açtık
-                    autoplay={{ delay: 2500, disableOnInteraction: false, }} // Otomatik geçiş
-                    pagination={{ clickable: true, }}
-                    // navigation={true}            
-                    modules={[Autoplay, Pagination]} // Kullanılan modüller
-                    className="mySwiper"
-                >
-                    <SwiperSlide>BangChan</SwiperSlide>
-                    <SwiperSlide>LeeKnow</SwiperSlide>
+            <div className="container max-w-none">
+                <Swiper className="mySwiper bg-gray-200">
+                    {/* Text ve Image iki ayrı columnda */}
+                    {/* Slide 1 */}
+                    <SwiperSlide className="text-black text-center flex items-center justify-center md:pt-5 md:bt-10">
+                        {/* StrayKids /} {/ Slide deneme texti */}
+                        <div className="container">
+                            <div className="grid grid-cols-2">
+                                {/* Left Column h1 and p */}
+                                <div className="">
+                                    <h1 className="text-8xl font-bold py-4 tracking-wide">You make StrayKids STAY</h1>
+                                    <p className="py-4">StrayKids Everywhere All Around The World!</p>
+                                </div>
+                                {/* Left Column h1 and p End */}
+
+                                {/* Rigth column Image */}
+                                <div className="">
+                                    <Image src="/skz_sty/skz_concert.png"
+                                        alt="Stray Kids Concert" // Next.js alt zorunlu
+                                        width={5500} // Genişlik ve yükseklik belirtilmeli
+                                        height={900}
+                                        className="rounded-lg shadow-lg"
+                                    />
+                                </div>
+                                {/* Rigth column Image End */}
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    {/* Slide 1 End */}
+
+                    {/* Slide 2 */}
+                    <SwiperSlide className="text-black text-center flex items-center justify-center md:pt-5 md:bt-10">
+                        <div className="container">
+                            <div className="grid grid-cols-2">
+                                {/* Left Column h1 and p */}
+                                <div className="">
+                                    <h1 className="text-8xl font-bold">SKZOO {" "}</h1>
+                                    <p>Wolf Chan, Leebit, Dwaekki, Jiniret, Han Quokka, BbokAri, PuppyM, FoxI.NY</p>
+
+                                    {/* Görselleri bu kısımda p etiketinin altında yerleştirdik */}
+                                    <div className="relative mt-4">
+                                        <Image
+                                            src="/skz_sty/wolf_chan.png"
+                                            alt="Wolf Chan"
+                                            width={80}
+                                            height={80}
+                                            className="absolute left-15 top-5 transform -translate-x-10 "
+                                        />
+                                        <Image
+                                            src="/skz_sty/leebit.png"
+                                            alt="Leebit"
+                                            width={80}
+                                            height={80}
+                                            className="absolute left-15 top-22 transform -translate-x-8 "
+                                        />
+                                        <Image
+                                            src="/skz_sty/dwaekki.png"
+                                            alt="Dwaekki"
+                                            width={50}
+                                            height={50}
+                                            className="absolute left-55 transform -translate-x-6 "
+                                        />
+                                        <Image
+                                            src="/skz_sty/jiniret.png"
+                                            alt="Jiniret"
+                                            width={80}
+                                            height={80}
+                                            className="absolute left-55 top-25 transform -translate-x-4 "
+                                        />
+                                        <Image
+                                            src="/skz_sty/han_quokka.png"
+                                            alt="Han Quokka"
+                                            width={80}
+                                            height={80}
+                                            className="absolute left-95 transform translate-x-4 "
+                                        />
+                                        <Image
+                                            src="/skz_sty/bbokkari.png"
+                                            alt="Bbokkari"
+                                            width={80}
+                                            height={80}
+                                            className="absolute left-95 top-25 transform translate-x-6 "
+                                        />
+                                        <Image
+                                            src="/skz_sty/puppy_m.png"
+                                            alt="Puppy M"
+                                            width={80}
+                                            height={80}
+                                            className="absolute left-125 top-5 transform translate-x-8 "
+                                        />
+                                        <Image
+                                            src="/skz_sty/foxi_ny.png"
+                                            alt="Foxi Ny"
+                                            width={95}
+                                            height={95}
+                                            className="absolute left-125 top-22 transform translate-x-10 "
+                                        />
+                                    </div>
+                                </div>
+                                {/* Left Column h1 and p End */}
+
+                                {/* Right Column Image */}
+                                <div className="">
+                                    <Image
+                                        src="/skz_sty/skzoo.png"
+                                        alt="Stray Kids Concert"
+                                        width={5495}
+                                        height={600}
+                                        className="rounded-lg shadow-lg"
+                                    />
+                                </div>
+                                {/* Right Column Image End */}
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    {/* Slide 2 End */}
+                    {/* Text ve Image iki ayrı columnda Kısmının sonu */}
                 </Swiper>
             </div>
         </>
--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -75,42 +75,43 @@ const Header = () => {
   }, [isNightMode]);
 
   return (
-    <header className="bg-white  container">
+    <header className="bg-white  container max-w-none mx-auto">
+      <div className="w-full bg-white dark:bg-black">
 
-      {/* Global Web Nav */}
-      <nav aria-label="Global" className="mx-auto flex items-center justify-between py-6">
+        {/* Global Web Nav */}
+        <nav aria-label="Global" className="mx-auto top-0 left-0 flex items-center justify-between py-6 px-6">
 
-        {/* logo side */}
-        <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
-            <Image
-              alt=""
-              src="/skz_sty/skz_logo2.png"
-              className=""
-              width={120}
-              height={80}
-            />
-          </a>
-        </div>
+          {/* logo side */}
+          <div className="flex lg:flex-1 ml-4">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <Image  
+                alt=""
+                src="/skz_sty/skz_logo.png"
+                className=""
+                width={120}
+                height={80}
+              />
+            </a>
+          </div>
 
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            onClick={() => setMobileMenuOpen(true)}
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon aria-hidden="true" className="size-6" />
-          </button>
-        </div>
-        {/* logo side end */}
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(true)}
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon aria-hidden="true" className="size-6" />
+            </button>
+          </div>
+          {/* logo side end */}
 
-        {/* Bar Sections */}
-        <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+          {/* Bar Sections */}
+          <PopoverGroup className="hidden lg:flex lg:gap-x-12">
 
-          {/* Product section */}
-          {/* <Popover className="relative">
+            {/* Product section */}
+            {/* <Popover className="relative">
             <PopoverButton className="flex items-center gap-x-1 text-sm/6 font-semibold text-gray-900">
               Product
               <ChevronDownIcon aria-hidden="true" className="size-5 flex-none text-gray-400" />
@@ -153,123 +154,125 @@ const Header = () => {
               </div>
             </PopoverPanel>
           </Popover> */}
-          {/* Product section end */}
+            {/* Product section end */}
 
-          <a href="#" className="text-lg font-medium text-gray-900">Members</a>
+            <a href="#" className="text-lg font-medium text-gray-900 dark:text-gray-200 transition-all">Members</a>
 
-          <a href="#" className="text-lg font-medium text-gray-900">SKZOO</a>
+            <a href="#" className="text-lg font-medium text-gray-900 dark:text-gray-200 transition-all">SKZOO</a>
 
-          <a href="#" className="text-lg font-medium text-gray-900">Albums</a>
+            <a href="#" className="text-lg font-medium text-gray-900 dark:text-gray-200 transition-all">Albums</a>
 
-          <a href="#" className="text-lg font-medium text-gray-900">Records</a>
+            <a href="#" className="text-lg font-medium text-gray-900 dark:text-gray-200 transition-all">Records</a>
 
-          <a href="#" className="text-lg font-medium text-gray-900 transition-all">Content</a>
+            <a href="#" className="text-lg font-medium text-gray-900 dark:text-gray-200 transition-all">Content</a>
 
-        </PopoverGroup>
-        {/* Bar Sections End */}
+          </PopoverGroup>
+          {/* Bar Sections End */}
 
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a
-            href="#"
-            className="text-sm/6 font-semibold text-gray-900"
-            onClick={handleToggle}
-          >
-            {isNightMode ? (
-              <Moon size={24} color="#000000" />
-            ) : (
-              <Sun size={24} color="#000000" />
-              // <Sun size={24} color="#FFFFFF" />
-            )}
-          </a>
-        </div>
+          {/* theme button */}
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end mr-4">
+            <a
+              href="#"
+              className="text-sm/6 font-semibold text-gray-900"
+              onClick={handleToggle}
+            >
+              {isNightMode ? (
+                <Sun size={24} color="#FFFFFF" />
+              ) : (
+                <Moon size={24} color="#000000" />
+                // <Sun size={24} color="#FFFFFF" />
+              )}
+            </a>
+          </div>
+          {/* theme button end */}
 
-        {/* Login Button */}
-        {/* <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          {/* Login Button */}
+          {/* <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           <a href="#" className="text-sm/6 font-semibold text-gray-900">
             Log in <span aria-hidden="true">&rarr;</span>
           </a>
         </div> */}
-        {/* Login Button End */}
+          {/* Login Button End */}
 
-      </nav>
-      {/* Global Web Nav End */}
+        </nav>
+        {/* Global Web Nav End */}
 
-      {/* Mobile Menu Dialog */}
-      <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-        <div className="fixed inset-0 z-10" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        {/* Mobile Menu Dialog */}
+        <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+          <div className="fixed inset-0 z-10" />
+          <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
 
-          {/* Menu Icon Bar */}
-          <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img
-                alt=""
-                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                className="h-8 w-auto"
-              />
-            </a>
-            <button
-              type="button"
-              onClick={() => setMobileMenuOpen(false)}
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-            >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon aria-hidden="true" className="size-6" />
-            </button>
-          </div>
-          {/* Menu Icon Bar End */}
-
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-
-              {/* Menu Bar Secttion */}
-              <div className="space-y-2 py-6">
-
-                {/* Product Section */}
-                <Disclosure as="div" className="-mx-3">
-                  <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
-                    Product
-                    <ChevronDownIcon aria-hidden="true" className="size-5 flex-none group-data-open:rotate-180" />
-                  </DisclosureButton>
-                  <DisclosurePanel className="mt-2 space-y-2">
-                    {[...products, ...callsToAction].map((item) => (
-                      <DisclosureButton
-                        key={item.name}
-                        as="a"
-                        href={item.href}
-                        className="block rounded-lg py-2 pr-3 pl-6 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50"
-                      >
-                        {item.name}
-                      </DisclosureButton>
-                    ))}
-                  </DisclosurePanel>
-                </Disclosure>
-                {/* Product Section End */}
-
-                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Features</a>
-
-                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Marketplace</a>
-
-                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Company</a>
-
-              </div>
-              {/* Menu Bar Secttion End */}
-
-              {/* Login Button */}
-              <div className="py-6">
-                <a
-                  href="#" className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Log in</a>
-              </div>
-              {/* Login Button End */}
-
+            {/* Menu Icon Bar */}
+            <div className="flex items-center justify-between">
+              <a href="#" className="-m-1.5 p-1.5">
+                <span className="sr-only">Your Company</span>
+                <img
+                  alt=""
+                  src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                  className="h-8 w-auto"
+                />
+              </a>
+              <button
+                type="button"
+                onClick={() => setMobileMenuOpen(false)}
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon aria-hidden="true" className="size-6" />
+              </button>
             </div>
-          </div>
+            {/* Menu Icon Bar End */}
 
-        </DialogPanel>
-      </Dialog>
-      {/* Mobile Menu Dialog End */}
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
 
+                {/* Menu Bar Secttion */}
+                <div className="space-y-2 py-6">
+
+                  {/* Product Section */}
+                  <Disclosure as="div" className="-mx-3">
+                    <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                      Product
+                      <ChevronDownIcon aria-hidden="true" className="size-5 flex-none group-data-open:rotate-180" />
+                    </DisclosureButton>
+                    <DisclosurePanel className="mt-2 space-y-2">
+                      {[...products, ...callsToAction].map((item) => (
+                        <DisclosureButton
+                          key={item.name}
+                          as="a"
+                          href={item.href}
+                          className="block rounded-lg py-2 pr-3 pl-6 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50"
+                        >
+                          {item.name}
+                        </DisclosureButton>
+                      ))}
+                    </DisclosurePanel>
+                  </Disclosure>
+                  {/* Product Section End */}
+
+                  <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Features</a>
+
+                  <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Marketplace</a>
+
+                  <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Company</a>
+
+                </div>
+                {/* Menu Bar Secttion End */}
+
+                {/* Login Button */}
+                <div className="py-6">
+                  <a
+                    href="#" className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Log in</a>
+                </div>
+                {/* Login Button End */}
+
+              </div>
+            </div>
+
+          </DialogPanel>
+        </Dialog>
+        {/* Mobile Menu Dialog End */}
+      </div>
     </header>
   )
 }
